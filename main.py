4e0b85f4dd0f16353eda8fc36f1807931942285f
[name] <[email address]>
2024-08-20 21:08:24 -0500

    Updated code to use env variable for username and github token

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 # first draft of visualizing my followers languages.
+# second draft trying to use as a github action.
+import os
 from github_api import get_followers, get_languages
 from data_processing import aggregate_languages
 from visualization import plot_language_distribution, plot_language_distribution_plotly
 
 def main():
-    username = "your_actual_github_username"
-    token = "your_actual_github_token"
+    username = os.getenv("FOLLOWERS_USERNAME")
+    token = os.getenv("FOLLOWERS_TOKEN")
+
+    if not token or not username:
+        raise ValueError("GitHub token or username not found. Please set the GITHUB_TOKEN and GITHUB_USERNAME environment variables.")
     
     followers = get_followers(username, token)
     language_data = aggregate_languages(followers, token)
